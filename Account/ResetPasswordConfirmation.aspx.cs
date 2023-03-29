67cbda54cddb405cfb5d03bdb8c94a85252345e2
John Winward <john.winward2@gmail.com>
2023-03-29 17:14:52 -0400

    create ASP.NET Web App from template

---
+++ Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace CPSC337_Project.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CPSC337_Project.Startup))]
+namespace CPSC337_Project
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
